02a26517ce2d61046c37cf8afe4fcd9544b9cf1d
Alexandre Cailler <alex02.cailler@gmail.com>
2024-04-20 00:51:01 +0200

    feat: add menu

--- components/the-header.vue
+++ components/the-header.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="header">
-    <div class="header__container">
-      <div>
-        The logo
+  <div>
+    <div class="header">
+      <div class="header__container">
+        <div>
+          The logo
+        </div>
+        <div>
+          <ul>
+            <li @click="openMenu">Menu</li>
+            <li><a href="mailto:alex02.cailler@gmail.com">Contact</a></li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <ul>
-          <li><a href="#">Menu</a></li>
-          <li><a href="mailto:alex02.cailler@gmail.com">Contact</a></li>
-        </ul>
-      </div>
+    </div>
+    <div :class="[`menu-overlay`, {
+      'is-active': menuIsOpen
+    }]">
+      <ul>
+        <li @click="closeMenu">Acceuil</li>
+        <li>A propos</li>
+        <li>Projets</li>
+        <li>Contact</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
+const menuIsOpen = ref(false)
 
+const openMenu = () => {
+  menuIsOpen.value = true
+}
+
+const closeMenu = () => {
+  menuIsOpen.value = false
+}
 </script>
 
 
@@ -29,17 +49,43 @@
     align-content: center;
   }
   ul {
-    padding: 0;
-    margin: 0;
     display: flex;
     align-items: center;
     gap: 8px;
     li {
-      list-style: none;
       a {
-        text-decoration: none;
         color: black;
       }
+    }
+  }
+}
+
+.menu-overlay {
+  position: fixed;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+  opacity: 0;
+  transition: all ease .5s;
+  display: flex;
+  align-items: center;
+  padding-left: 15%;
+  &.is-active {
+    z-index: 1000;
+    opacity: 1;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.90);
+    backdrop-filter: blur(15px);
+
+    top: 0;
+  }
+  ul {
+    li {
+      line-height: 100%;
+      font-size: 9rem;
+      font-weight: 600;
+      color: white;
     }
   }
 }
